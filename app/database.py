26a025093f217d5ca54f842ed69026396738660e
scarasatti <scarasatti@gmail.com>
2025-06-12 20:09:51 -0300

    debug: corigindo configuração do database

--- app/database.py
+++ app/database.py
@@ -1,19 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("Variável DATABASE_URL não configurada")
 
-# Conexão com SQLite (para testes locais)
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    echo=True,
+    connect_args={"check_same_thread": False}  # somente para SQLite
+    if SQLALCHEMY_DATABASE_URL.startswith("sqlite")
+    else {}
 )
 
-# Gerenciador de sessões
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base usada para declarar os modelos
 Base = declarative_base()
 
 def get_db():
